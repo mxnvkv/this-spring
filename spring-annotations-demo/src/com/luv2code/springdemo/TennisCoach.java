fab44b338dd00bae8f92b9b11502847b1e621ce3
Maksym Novikov <maksymn@suntechinnovation.com>
2022-01-06 14:14:06 +0200

    8-spring-configuration-with-java-annotations-dependency-injection: any setter method name

--- spring-annotations-demo/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-annotations-demo/src/com/luv2code/springdemo/TennisCoach.java
@@ -18,8 +18,8 @@ public class TennisCoach implements Coach {
   // }
 
   @Autowired
-  public void setFortuneService(FortuneService fortuneService) {
-    System.out.println(">> TennisCoach: inside setFortuneService");
+  public void doSomeCrazyStuff(FortuneService fortuneService) {
+    System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
     this.fortuneService = fortuneService;
   }
 
