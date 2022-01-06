d4203cda6652e0a5231cf15a90f75315bb111ecf
Maksym Novikov <maksymn@suntechinnovation.com>
2022-01-06 20:36:37 +0200

    8-spring-configuration-with-java-annotations-dependency-injection: define scope with annotation

--- spring-annotations-demo/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-annotations-demo/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,10 +1,12 @@
 package com.luv2code.springdemo;
 
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
   @Autowired
