2c4be726179ae19561ae5c444b8a58fce80adaf0
Maksym Novikov <maksymn@suntechinnovation.com>
2022-01-09 16:56:50 +0200

    12-spring-mvc-creating-controllers-and-views: app is finally configured!

--- spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HomeController.java
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-  @RequestMapping("/main-menu")
+  @RequestMapping("/")
   public String showPage() {
     return "main-menu";
   }
