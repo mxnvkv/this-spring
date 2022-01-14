70b4601db3c4909693ae4c4dee7914b64d9abec2
Maksym Novikov <maksymn@suntechinnovation.com>
2022-01-14 22:45:45 +0200

    14-spring-mvc-form-tags-and-data-binding: @ModelAttribute

---
+++ spring-mvc-demo/src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,28 @@
+package com.luv2code.springdemo.mvc;
+
+public class Student {
+
+  private String firstName;
+  private String lastName;
+
+  public Student() {
+
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+}
