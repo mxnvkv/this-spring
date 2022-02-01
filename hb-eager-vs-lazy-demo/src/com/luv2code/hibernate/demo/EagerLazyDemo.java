e2663551a01f63a02ea9913e1f27d662729bab97
Maksym Novikov <maksymn@suntechinnovation.com>
2022-02-01 23:23:11 +0200

    25-hibernate-advanced-mappings-eager-vs-lazy-loading: eager vs lazy

---
+++ hb-eager-vs-lazy-demo/src/com/luv2code/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class EagerLazyDemo {
+
+  public static void main(String[] args) {
+
+    // create session factory
+    SessionFactory sessionFactory = new Configuration()
+      .configure("hibernate.cfg.xml")
+      .addAnnotatedClass(Instructor.class)
+      .addAnnotatedClass(InstructorDetail.class)
+      .addAnnotatedClass(Course.class)
+      .buildSessionFactory();
+
+    // create session
+    Session session = sessionFactory.getCurrentSession();
+
+    try {
+      // start a transaction
+      session.beginTransaction();
+
+      // get the instructor from db
+      int theId = 1;
+      Instructor tempInstructor = session.get(Instructor.class, theId);
+
+      System.out.println("luv2code: Instructor: " + tempInstructor);
+
+      // get courses for the instructor
+      System.out.println("luv2code: Courses: " + tempInstructor.getCourses());
+
+      // commit transaction
+      session.getTransaction().commit();
+
+      System.out.println("Done!");
+    }
+    finally {
+      // add clean up code
+      session.close();
+
+      sessionFactory.close();
+    }
+  }
+
+}
