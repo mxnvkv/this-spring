c278fca3adf90cb2d807d6a9f9099bd0bb4b9591
Maksym Novikov <maksymn@suntechinnovation.com>
2022-02-04 22:25:12 +0200

    28-build-a-database-web-app-spring-mvc-and-hibernate-project-part-1: created controller and DAO interface

--- web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.luv2code.springdemo.entity.controller;
+package com.luv2code.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
