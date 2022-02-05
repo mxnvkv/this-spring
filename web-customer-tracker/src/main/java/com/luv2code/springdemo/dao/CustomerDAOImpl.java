de48dc23eb89f51a8ac9a687ad766acb8ee6b51f
Maksym Novikov <maksymn@suntechinnovation.com>
2022-02-05 18:23:44 +0200

    31-build-a-database-web-app-spring-mvc-and-hibernate-project-part-4: save customer

--- web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -28,4 +28,11 @@ public class CustomerDAOImpl implements CustomerDAO {
     return customers;
   }
 
+  @Override
+  public void saveCustomer(Customer customer) {
+    Session session = sessionFactory.getCurrentSession();
+
+    session.save(customer);
+  }
+
 }
