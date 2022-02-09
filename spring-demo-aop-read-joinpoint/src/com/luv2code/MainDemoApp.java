745c97665a0a82927446b6da05bb80dbc481bfc9
Maksym Novikov <maksymn@suntechinnovation.com>
2022-02-09 21:44:32 +0200

    40-aop-joinpoints: created new project

---
+++ spring-demo-aop-read-joinpoint/src/com/luv2code/MainDemoApp.java
@@ -0,0 +1,44 @@
+package com.luv2code;
+
+import com.luv2code.aopdemo.Account;
+import com.luv2code.aopdemo.DemoConfig;
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+  public static void main(String[] args) {
+
+    // read spring config java class
+    AnnotationConfigApplicationContext context =
+      new AnnotationConfigApplicationContext(DemoConfig.class);
+
+    // get the bean from spring container
+    AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+    // get membership bean from spring container
+    MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+    // call the business method
+    Account account = new Account();
+    accountDAO.addAccount(account, true);
+    accountDAO.doWork();
+
+    // call the accountDAO getter/setter methods
+    accountDAO.setName("foobar");
+    accountDAO.setServiceCode("silver");
+
+    String name = accountDAO.getName();
+    String code = accountDAO.getServiceCode();
+
+    // call the membership business method
+    membershipDAO.addSillyMember();
+    membershipDAO.goToSleep();
+
+    // close the context
+    context.close();
+
+  }
+
+}
