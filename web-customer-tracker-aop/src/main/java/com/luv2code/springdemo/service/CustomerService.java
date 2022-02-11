dccace8307a2971affd7b7d2102a51836cb89682
Maksym Novikov <maksymn@suntechinnovation.com>
2022-02-11 22:49:02 +0200

    45-aop-add-aop-logging-to-spring-mvc-app-real-time-project: created new project

---
+++ web-customer-tracker-aop/src/main/java/com/luv2code/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo.service;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+  public Customer getCustomer(int id);
+
+  public List<Customer> getCustomers();
+
+  public void saveCustomer(Customer customer);
+
+  public void deleteCustomer(int id);
+
+}
