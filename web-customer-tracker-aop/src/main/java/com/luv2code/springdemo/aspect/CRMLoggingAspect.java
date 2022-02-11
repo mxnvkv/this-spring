fd23853c7e73ecf35876d454077f3a9a8b1f52e4
Maksym Novikov <maksymn@suntechinnovation.com>
2022-02-11 23:04:47 +0200

    45-aop-add-aop-logging-to-spring-mvc-app-real-time-project: created @Pointcuts

--- web-customer-tracker-aop/src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/main/java/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.springdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -13,6 +14,18 @@ public class CRMLoggingAspect {
   private Logger logger = Logger.getLogger(getClass().getName());
 
   // setup pointcut declarations
+  @Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+  private void forControllerPackage() { }
+
+  // do the same for service and DAO
+  @Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
+  private void forServicePackage() { }
+
+  @Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
+  private void forDaoPackage() { }
+
+  @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+  private void forAppFlow() { }
 
   // add @Before advice
 
